779a307965e4a14184765f5cbc56ef80de94cf62
StevenJNPearce <stevenjnpearce@gmail.com>
2019-05-31 20:00:24 +0700

    circleci config test

--- bin/myb_tracker.ts
+++ bin/myb_tracker.ts
@@ -4,4 +4,4 @@ import cdk = require('@aws-cdk/cdk');
 import { MybTrackerStack } from '../lib/myb_tracker-stack';
 
 const app = new cdk.App();
-new MybTrackerStack(app, 'MybTrackerStackd');
+new MybTrackerStack(app, 'TrackerStack', { env: { region: 'us-east-1' }});
